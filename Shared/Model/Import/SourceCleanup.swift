25f61ecb994c5b414bbd33ea89294ec8f4d2afdc
Pierre Gabory <piergabory@icloud.com>
2020-12-17 17:33:01 +0100

    save clean changes

--- Shared/Model/Import/SourceCleanup.swift
+++ Shared/Model/Import/SourceCleanup.swift
@@ -7,33 +7,58 @@
 
 import Foundation
 import CoreData
+import Combine
 
 struct SourceCleanup {
+    private static let processingQueue = DispatchQueue(label: "LocationCleanup")
+    
     let managedObjectContext: NSManagedObjectContext
     let source: Source
-    
+    var totalLocationCount: Int { source.locations?.count ?? 0 }
+    let locationsProcessedCount = CurrentValueSubject<Int, Never>(0)
+
     func cleanSource() {
+        SourceCleanup.processingQueue
+            .async(execute: task)
+    }
+    
+    // Parse through the source in order
+    // remove null coordinates, null timestamps
+    // remove teleporting, no movements
+    // remove faster than sound movement, as its unlikely the user
+    // maintains internet access while flying a fighter jet
+    private func task() {
         let locations = source.sortedLocations
         var invalidated: Set<Location> = []
+    
+        print("Start \(source.name ?? "") cleanup")
         for index in locations.indices[1...] {
             let current = locations[index]
             guard
-                current.latitude != 0 && current.longitude != 0,
-                current.timestamp?.timeIntervalSince1970 != 0 else {
+                abs(current.latitude) > 0.001 && abs(current.longitude) > 0.001 else {
                     invalidated.insert(current)
                     continue
             }
+            
             let previous = locations[index - 1]
             guard
-                previous.time(from: current) > 0, // discard teleportation
-                previous.distance(from: current) > 0, // discard no movement
-                previous.velocity(to: current) > 300 else { // discard faster than sound, probably a fluke
+                previous.time(from: current) > 0,
+                previous.distance(from: current) > 0,
+                previous.velocity(to: current) < 300 else {
                     invalidated.insert(current)
                     continue
             }
+            
+            locationsProcessedCount.value = index
         }
+        print("Saving  \(source.name ?? "") cleanup")
+        print("remove \(invalidated.count) out of \(totalLocationCount)")
+        
         managedObjectContext.perform {
+            invalidated.forEach(managedObjectContext.delete)
             source.removeFromLocations(invalidated as NSSet)
+            do { try managedObjectContext.save() } catch { print(error) }
+            print("Finished  \(source.name ?? "") cleanup")
         }
     }
 }
