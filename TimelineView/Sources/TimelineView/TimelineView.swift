29804d9a220fe4e86164cfaa9da4ecf5cb52baaa
Pierre Gabory <piergabory@icloud.com>
2023-05-24 23:48:39 +0200

    Time Timeline

--- TimelineView/Sources/TimelineView/TimelineView.swift
+++ TimelineView/Sources/TimelineView/TimelineView.swift
@@ -8,16 +8,39 @@
 import SwiftUI
 
 struct TimelineView: View {
-    let span = -1000...1000
+    let dateInterval: DateInterval
+    let markInterval: TimeInterval
+    
+    var timelineData: [Date] {
+        Array(stride(from: dateInterval.start, to: dateInterval.end, by: markInterval))
+    }
+    
+    var origin: Date {
+        min(max(dateInterval.start, .now), dateInterval.end)
+    }
+    
+    var format: Date.FormatStyle {
+        switch markInterval {
+        case ..<60:
+            return Date.FormatStyle(date: .none, time: .standard)
+        case 60..<(3600 * 24):
+            return Date.FormatStyle(date: .none, time: .shortened)
+        default:
+            return Date.FormatStyle(date: .abbreviated, time: .none)
+        }
+    }
     
     var body: some View {
-        Ruler(span, format: .number, origin: 0)
+        Ruler(timelineData, format: format, origin: origin)
     }
 }
 
 struct TimelineView_Previews: PreviewProvider {
     static var previews: some View {
-        TimelineView()
+        TimelineView(
+            dateInterval: DateInterval(start: .now - 1000000, end: .now + 1000000),
+            markInterval: 400000
+        )
             .previewLayout(.fixed(width: 300, height: 80))
     }
 }
